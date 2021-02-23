feb2271513672c52d1b7734350a074a0cbaa6d85
tongruixun <1064044342@qq.com>
2021-02-23 14:26:41 +0800

    【新增】查询接口

---
+++ app/controller/user.js
@@ -0,0 +1,12 @@
+const Controller = require('egg').Controller;
+
+class UserController extends Controller {
+    async info() {
+        const ctx = this.ctx;
+        const userId = ctx.params.id;
+        const user = await ctx.service.user.find(userId);
+        ctx.body = user;
+    }
+}
+
+module.exports = UserController;
---
+++ config/plugin.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/** @type Egg.EggPlugin */
+module.exports = {
+    mysql: {
+        enable: true,
+        package: 'egg-mysql',
+    }
+  // had enabled by egg
+  // static: {
+  //   enable: true,
+  // }
+};
